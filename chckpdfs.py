23e8431135286ec78aa80554c30e56f68963208a
edombek <edombek@yandex.ru>
2022-10-17 23:06:49 +0300

    fix

--- chckpdfs.py
+++ chckpdfs.py
@@ -12,5 +12,5 @@ with open('index.html') as file:
     for i, line in enumerate(file.readlines()):
         if "href=" in line and "?file=https://raw.githubusercontent.com/rf-slf-4x1/site_materials/main/" in line and ".pdf" in line:
             file = line.split("?file=https://raw.githubusercontent.com/rf-slf-4x1/site_materials/main/")[1].split('"')[0]
-            if not os.path.isfile('/data/projects/site_materials/' + file):
+            if not os.path.isfile('../site_materials/' + file):
                 print(f'"{file}"')
